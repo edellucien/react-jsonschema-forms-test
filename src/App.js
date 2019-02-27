b0206bb0dd0f9f24d013dbd5940b8d33c70984f8
lucien <lucien.edel@geops.de>
2019-02-27 09:26:59 +0100

    Initial commit for 'react-jsonschema-forms-test'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from "react-jsonschema-form";
+
+class FormWrapper extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: [],
+      schema: {},
+      uiSchema: {},
+      formData: {},
+    };
+
+    const GET_SCHEMA = '/api/infofpw/messtisch/form/';
+    const GET_RESULT = `${GET_SCHEMA}?format=json`;
+
+    this.fetchSchema(GET_SCHEMA);
+    this.fetchResult(GET_RESULT);
+  }
+
+  fetchSchema(url) {
+    fetch(url, {
+      method: 'OPTIONS',
+    }).then(response => {
+      if (response.ok) {
+        response.json().then(json => {
+          this.setState({
+            schema: json.schema,
+            uiSchema: json.ui,
+          });
+          ;
+        })
+      }
+    })
+  }
+
+  fetchResult(url) {
+    fetch(url, {
+      method: 'GET',
+    }).then(data => {
+      if (data.ok) {
+        data.json().then(json => {
+          this.setState({
+            results: json.results,
+            formData: json.results[0],
+          });
+        });
+      }
+    });
+  }
+
+  render() {
+    if (this.state.formData) {
+      const listResults = this.state.results.map((item) =>
+        <li key={item.id}>{item.id} - {item.bezeichnung}</li>);
+
+      return (
+        <div className="App-form-wrapper">
+          <div className="App-results-wrapper">
+            Results:
+            <ul>{listResults}</ul>
+          </div>
+
+          Form:
+          <Form
+            schema={this.state.schema}
+            uiSchema={this.state.uiSchema}
+            formData={this.state.formData}
+            className="App-form"
+          />
+        </div>
+      );
+    }
+    return null;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <FormWrapper />
         </header>
       </div>
     );
